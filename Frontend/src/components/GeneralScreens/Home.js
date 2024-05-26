b4396ac9249e19ff3f3d24b041e6ab18eeb24a34
Mohammmedrafique <pushpendrasharmakarauli@gmail.com>
2024-05-26 12:53:13 +0530

    F

--- Frontend/src/components/GeneralScreens/Home.js
+++ Frontend/src/components/GeneralScreens/Home.js
@@ -107,6 +107,7 @@ import { v4 as uuidv4 } from 'uuid';
 import { useState, useEffect } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import SkeletonStory from "../Skeletons/SkeletonStory";
+import api from "../../api";
 import CardStory from "../StoryScreens/CardStory";
 import NoStories from "../StoryScreens/NoStories";
 import Pagination from "./Pagination";
